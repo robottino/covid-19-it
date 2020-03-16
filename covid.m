dd39482a07fe4af90b01b9fc845d9b27fdfb2313
Roberto Casati <casati.roberto@gmail.com>
2020-03-16 13:50:59 +0100

    small improvements

--- covid.m
+++ covid.m
@@ -5,10 +5,10 @@ pkg load optim
   
   F = @(x,p) p(1) + p(2) * exp(p(3) * (x - p(4)))./(1 + exp(p(3) * (x - p(4))));
  
-  %% independents and dependents:
-%%  data = load('/home/roberto/octave/covid19-it.txt');
-  data = load('data/covid19-it.txt');
-  x = data(:,1);y = data(:,2);
+  data = load('data/covid-19-data-it.txt');
+  %%data = load('data/covid19-it.txt');
+  x = data(:,1);
+  y = data(:,2);
 
   p1=min(y);
   p2=max(y)-min(y);
@@ -19,27 +19,30 @@ pkg load optim
   %init = [50000.0; 30.0; 0.2];
   init=[p1,p2,p3,p4];
   %% other configuration (default values):
-  tolerance = .0000000001;
+  tolerance = .00000001;
   max_iterations = 200;
   weights = ones (size (y));
   dp = [.00001; .00001; .00001; .00001]; % bidirectional numeric gradient stepsize
   dFdp = "dfdp"; % function for gradient (numerical)
   
-   minstep = [0.01; 0.01;0.01; 0.01];
- maxstep = [0.8; 0.8; 0.8; 0.8];
+ minstep = [0.001; 0.001;0.001; 0.001];
+ maxstep = [0.08; 0.08; 0.08; 0.08];
  options = [minstep, maxstep];
-
-  %% linear constraints, A.' * parametervector + B >= 0
-  %%A = [1; -1]; B = 0; % p(1) >= p(2);
-  %%options.inequc = {A, B};
 
   %% start leasqr, be sure that 'verbose' is not set
   global verbose; verbose = false;
   [f, p, cvg, iter] = ...
       leasqr (x, y, init, F, tolerance, max_iterations,weights,dp,dFdp)
       
-      xx=[0:60]';
+      xx=[0:80]';
       
- plot(xx,F(xx,p),x,y,'or');
- xlabel('Giorni dal 15/2');ylabel('Casi totali');
+ 
+ plot(xx,F(xx,p),'linewidth',4,x,y,'or','linewidth',2);
+ 
+ xlabel('Days since January 22, 2020');
+ ylabel('Total cases');
+ set (gca, "xgrid", "on");
+ set (gca, "ygrid", "on");
+ 
+ axis([20 80 0 70000]);
  
