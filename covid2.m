6393d7d700bf628299c3305aeb34a17cdf4e699b
Roberto Casati <casati.roberto@gmail.com>
2020-03-17 12:15:56 -0400

    beta and N estimation WIP

--- covid2.m
+++ covid2.m
@@ -1,10 +1,8 @@
 pkg load optim
 
   function params = estimate (F, x, y, init)
-    global verbose; verbose = false;
-    [f, p, cvg, iter] = ...
-      leasqr (x, y, init, F);
-    params=p;      
+    [f, p, cvg, iter] = leasqr (x, y, init, F);
+    params = p;
   endfunction
 
 function xdot = f (x,t)
@@ -20,25 +18,11 @@ endfunction
 
 function xdot = sir(x,t)
   N = x(1)+x(2)+x(3);
-  beta = 0.00001582;
-  gamma = 0.00000001;
+  beta = 0.22683/N;
+  gamma = 1000/N;
   
   %% S
-  xdot(1) = - (beta * x(2) * x(1)) / N;
-
-  %% I
-  xdot(2) = (beta * x(2) * x(1)) / N - gamma * x(2);
-
-  %% R
-  xdot(3) = gamma * x(2);
-endfunction
-
-function xdot = sir2(x,t)
-  beta = 6.8958e+04;
-  gamma = 0.5;
-  
-  %% S
-  xdot(1) = - beta * x(2) * x(1);
+  xdot(1) = - beta* x(2) * x(1);
 
   %% I
   xdot(2) = (beta * x(2) * x(1)) - gamma * x(2);
@@ -48,20 +32,34 @@ function xdot = sir2(x,t)
 endfunction
 
 %%x = lsode ("f", [1; 2], (t = linspace (0, 50, 50)')); plot(t,x);
- x = lsode ("sir2", [15000; 3; 0], (t = linspace (0, 100, 200)')); plot(t,x,'linewidth',2);
+t = linspace (0, 100, 200)';
+ x = lsode ("sir", [66318.20533; 0.22683; 0], t); 
 
 %%  logistica
-I = @(x,p) p(1)./(exp(p(2) * x + p(1)) - 1) + p(1)
- 
+%%I = @(x,p) p(1)./(exp(p(2) * x + p(1)) - 1) + p(1)
+
+%% L / (1+ exp(-k(x-x0))); L=p(1), k=p(2), x0=p(3)
+I = @(x,p) p(1) ./ (1+exp(-p(2)*(x-p(3)))); init_I=[0,0,0];
+
+#{ 
 data_china = load('data/covid-19-data-china.txt');
 x_china = data_china(:,1);
 y_china = data_china(:,2);
-p_china = estimate(I,x_china,y_china,[0.1,0.1]);
+p_china = estimate(I,x_china,y_china,init);
+#}
 
 data_it = load('data/covid-19-data-it.txt');
 x_it = data_it(:,1);
 y_it = data_it(:,2);
-p_it = estimate(I,x_it,y_it,[1,1]);
+%%p_it = estimate(I,x_it,y_it,init_I);
+[f_it, p_it, cvg_it, iter_it] = leasqr (x_it, y_it, init_I, I);
 
-disp(p_china);
+%%disp(p_china);
 disp(p_it);
+
+plot(x_it,y_it,'linewidth',2,'o',
+  %%t,I(t,p_it),'linewidth',2,
+  t,x(:,2) + x(:,3),
+  t,x);
+ set (gca, "xgrid", "on");
+ set (gca, "ygrid", "on");
