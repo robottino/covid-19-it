2c21e7a7c780acde42eb701ba32f0fb2065ce4b3
Roberto Casati <casati.roberto@gmail.com>
2020-03-19 23:47:50 +0100

    auto updat data

---
+++ data/update-ita.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mv "dpc-covid19-ita-andamento-nazionale.csv" "dpc-covid19-ita-andamento-nazionale-$(date +"%m-%d-%Y").csv"
+curl "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv" | cut -d\, -f3- | tail -n +2 > dpc-covid19-ita-andamento-nazionale.csv
