7dd30447c3bc6fe280a0c32cef589326c7a0c82f
Roberto Casati <casati.roberto@gmail.com>
2020-05-12 22:36:17 +0200

    clean

--- data/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Downloading..."
-curl "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv" > covid-19-data.csv
-echo "...done."
-
-cat covid-19-data.csv | grep Italy | cut -d\, -f5- | sed 's/,/\n/g' | (i=0; while read line; do echo $((i++))','$line; done) > covid-19-data-it.txt
-
-
